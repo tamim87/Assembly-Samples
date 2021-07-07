abce1cc097e94ac10fbd697bcc406ab47f821d64
tamim <noormuhammadtamim@gmail.com>
2021-07-07 12:39:42 +0600

    deleted unnecessary files

--- numofsetbits.asm
+++ numofsetbits.asm
@@ -1,24 +1,27 @@
+;number of setbits    
+;credits: nabil faiyaz sadi
+
 org 100h
 
-.data
-num db 0FFh
-setBit db 0h
-
-.code
 mov cx, 8h
 mov al, num
 
-outer:
+for:
     mov bl, al
     mov dl, 1h
     and bl, dl
     cmp bl, 1h
     je inc_set_bits
-    back_to_outer:
+    resumeloop:
         shr al, 1
-        loop outer
+        loop for
 ret
 
 inc_set_bits:
-    inc setBit
-    jmp back_to_outer
+    inc setBitCount
+    jmp resumeloop   
+
+
+num db 0FFh
+setBitCount db 0h   
+       
